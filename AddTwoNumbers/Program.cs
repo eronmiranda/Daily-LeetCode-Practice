255fc620856a3461d36a613c3fb460de6c21673f
eronmiranda <49223433+eronmiranda@users.noreply.github.com>
2022-06-18 18:31:30 -0600

    Practice Day 2

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            l1
+            return null;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -8,6 +8,7 @@ namespace TwoSum
         static void Main(string[] args)
         {
             int[] nums = { 3, 2, 4 };
+            Console.Write("Enter target value: ");
             int target = int.Parse(Console.ReadLine());
             foreach (var item in TwoSum(nums, target))
             {
@@ -18,24 +19,26 @@ namespace TwoSum
         // Change and improve this.
         public static int[] TwoSum(int[] nums, int target)
         {
-            Dictionary<int, int> hashMap = new Dictionary<int, int>();
-
-            Array.Sort(nums);
-
-            for (int i = 0; i < nums.Length; i++)
+            Dictionary<int, int> numsDictionary = new Dictionary<int, int>();
+            
+            if(nums != null && nums.Length >=2)
             {
-                int complement = target - nums[i];
-                if (hashMap.ContainsKey(complement))
+                for (int i = 0; i < nums.Length; i++)
                 {
-                    return new int[] { hashMap[complement], i};
-                }
-                else
-                {
-                    hashMap.Add(nums[i], i);
+                    int complement = target - nums[i];
+                    //if (hashMap.ContainsKey(complement))
+                    //{
+                    //    return new int[] { hashMap[complement], i };
+                    //}
+                    if(numsDictionary.TryGetValue(complement, out int index))
+                    {
+                        return new[] { index, i };
+                    }
+                    numsDictionary[nums[i]] = i;
                 }
             }
 
-            return null;
+            return Array.Empty<int>();
         }
     }
 }
